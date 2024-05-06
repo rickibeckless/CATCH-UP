5b5155b0a277163f9c81a60ab12f9a88b7d2fd20
Ricki Beckless <glbeckless@hotmail.com>
2024-05-06 02:47:45 -0500

    feat: finish home styling and add app blog stats

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Created by: **Ricki Beckless**
 
 This web app: **CATCH-UP! Project Description...**
 
-Time spent: **78** hours spent in total
+Time spent: **80** hours spent in total
 
 <!-- ## Features
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,10 @@ export function Home() {
 
     const { id } = useParams();
     const [posts, setPosts] = useState([]);
-    const [comment, setComment] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [upvotes, setUpvotes] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [wordCount, setWordCount] = useState([]);
     const [previewAtTop, setPreviewAtTop] = useState(false);
 
     useEffect(() => {
@@ -24,13 +27,42 @@ export function Home() {
                 const slicedData = postData.slice(0, 5);
                 const sortedData = slicedData.sort((a, b) => b.upvotes - a.upvotes);
 
+                console.log("Posts:", sortedData);
+
+                const totalComments = postData.reduce((acc, post) => acc + post.comments, 0);
+                setComments(totalComments);
+
+                const totalUpvotes = postData.reduce((acc, post) => acc + post.upvotes, 0);
+                setUpvotes(totalUpvotes);
+
+                const totalWords = postData.reduce((acc, post) => acc + post.content.split(' ').length, 0);
+                setWordCount(totalWords);
+
                 setPosts(sortedData);
             } catch (error) {
                 console.error("Error fetching posts:", error.message);
             }  
         };
 
+        const fetchUsers = async () => {
+            try {
+                const { data: userData, error: userError } = await supabase
+                    .from('Users')
+                    .select('*');
+                if (userError) {
+                    throw userError;
+                }
+
+                console.log("Users:", userData);
+
+                setUsers(userData);
+            } catch (error) {
+                console.error("Error fetching users:", error.message);
+            }
+        };
+
         fetchPosts();
+        fetchUsers();
     }, [supabase, id]);
 
     useEffect(() => {
@@ -60,8 +92,8 @@ export function Home() {
                     {posts?.map(post => (
                         <div key={post._id} className="home-preview-post">
                             <div className="preview-post-heading">
-                                <h2>{post.title}</h2>
-                                <Link to={`/user/${post.user_username}`}>{post.user_username}</Link>
+                                <Link className="home-preview-post-title" to={`/post/${post.id}`}>{post.title}</Link>
+                                <Link className="home-preview-post-links" to={`/user/${post.user_username}`}>{post.user_username}</Link>
                             </div>
                             <div className="preview-post-stats">
                                 <p>Upvotes: {post.upvotes}</p>
@@ -72,17 +104,44 @@ export function Home() {
                                     <>
                                         {`${parse(DOMPurify.sanitize(post.content).replace(/<[^>]+>/g, '')).slice(0,400)}...`} 
                                         <span className="preview-post-read-more">
-                                            <Link to={`/post/${post.id}`}>read more.</Link>
+                                            <Link className="home-preview-post-links" to={`/post/${post.id}`}>read more</Link>
                                         </span>
                                     </>
                                     : parse(DOMPurify.sanitize(post.content).replace(/<[^>]+>/g, '')
                                 )}
                             </p>
-                            
                         </div>
                     ))}
                 </div>
-                <h4 id="blog-preview-stats">blah</h4>
+                <div id="blog-preview-stats-holder">
+                    <h3 id="blog-preview-stats-header">Total Number of: </h3>
+                    <div id="blog-preview-stats">
+                        <div className="blog-preview-stats-totals">
+                            <p>Users: </p>
+                            <h4>{users.length}</h4>
+                        </div>
+
+                        <div className="blog-preview-stats-totals">
+                            <p>Posts: </p>
+                            <h4>{posts.length}</h4>
+                        </div>
+
+                        <div className="blog-preview-stats-totals">
+                            <p>Upvotes: </p>
+                            <h4>{upvotes}</h4>
+                        </div>
+
+                        <div className="blog-preview-stats-totals">
+                            <p>Comments: </p>
+                            <h4>{comments}</h4>
+                        </div>                        
+                    </div>
+                    <h4 id="blog-preview-stats-words">
+                        And 
+                        <span id="blog-preview-stats-word-count"> {wordCount} </span>
+                        words total!
+                    </h4>
+                </div>
             </main>
         </>
     )
